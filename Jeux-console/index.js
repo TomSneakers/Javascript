b173642f3a848c21ecb2a5a7a675ebb717388926
TomSneakers <tomdesvignes031@gmail.com>
2022-12-22 11:51:15 +0100

    Jeux-console

--- Jeux-console/index.js
+++ Jeux-console/index.js
@@ -22,3 +22,43 @@ function jeu() {
     launchGame(answer);
   })
 }
+//fonction du debut de la partie
+function launchGame(name) {
+  let bet;
+  //demande d'entrée pile ou face au joeur 
+  rl.question(chalk.greenBright('Pile ou face ? '), answer => {
+    if (answer == "pile") bet = 0;
+    if (answer == "face") bet = 1;
+    //Si l'utilisateur entre une autre chaine de caractère que "pile" ou "face" alors un message d'erreur s'affiche et il perd automatiquement
+    else console.log((chalk.blue)`l'entrer n'est pas correcte, veuillez réessayer`)
+    const result = getResult(bet);
+
+    savePartie(name, answer, result);
+    exitOrReplay();
+  });
+}
+//fonction pour donne le resultat
+function getResult(bet) {
+  //variable avec fonction qui permet de determiner un chiffre aléatoire entre 0 et 1
+  const pileOuFace = Math.round(Math.random());
+  //Si la variable pilOuFace déterminer aleatoirement à la ligne 40 contient la même chaine de caractère que bet alors il retourne winner sinon perdu
+  if (pileOuFace == bet) {
+    console.log(`
+  ╔═══╦╦════════╗
+  ║╔╦╦╬╬═╦═╦═╦═╗║
+  ║║║║║║║║║║╩╣╠╝║
+  ║╚══╩╩╩╩╩╩═╩╝ ║
+  ╚═════════════╝
+    `);
+    return "win";
+  }
+  else {
+    console.log(`
+    ╔═══════╦╦═══╗
+    ║╔═╦═╦═╦╝╠╗╔╗║
+    ║║║║╩╣╠╣║║╚╝║║
+    ║║╔╩═╩╝╚═╩══╝║
+    ╚╩╩══════════╝`)
+    return "loose";
+  }
+}
